e6183c90c3102c784a8f7664655c280a827f301b
Jeffery Garza <jefferyagarza@gmail.com>
2014-03-01 23:08:58 -0600

    Add Euler Problem 2 Solution

---
+++ javascript/project_euler/euler2.js
@@ -0,0 +1,3 @@
+// Project Euler: Problem 2
+// Project URL: http://projecteuler.net/problem=2
+// Problem: Find the sum of even valued terms in a Fibonacci sequence whose values do not exceed four million.
