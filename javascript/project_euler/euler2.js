064eb54098ac903faa5aab47e4ae7ec3ba424126
Jeffery Garza <jefferyagarza@gmail.com>
2014-03-01 23:10:22 -0600

    Fix Empty File

--- javascript/project_euler/euler2.js
+++ javascript/project_euler/euler2.js
@@ -1,3 +1,24 @@
 // Project Euler: Problem 2
 // Project URL: http://projecteuler.net/problem=2
 // Problem: Find the sum of even valued terms in a Fibonacci sequence whose values do not exceed four million.
+
+//Calculate the Fib. sequence from 0 - 4,000,000
+var a = 0;
+var b = 1;
+var c = 0;
+var sum = 0;
+
+while( a < 4000000) {
+    
+    if( a % 2 === 0){
+        sum += a;
+    }
+    c = a + b;
+    a = b;
+    b = c;
+}
+
+console.log(sum);
+
+
+
